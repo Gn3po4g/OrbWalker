7d906b84c64a8bdc84814513a1d3cc05d06aaa87
Noroby <gnepoag@outlook.com>
2023-03-11 20:36:19 +0800

    Use cmake and libmem.

--- src/functions.cpp
+++ src/functions.cpp
@@ -24,9 +24,10 @@ namespace Functions {
     }
 
     uint64_t GetGameTime() {
-        static const auto start_time = std::chrono::steady_clock::now();
-        const auto elapsed_time = std::chrono::steady_clock::now() - start_time;
-        return std::chrono::duration_cast<std::chrono::milliseconds>(elapsed_time).count();
+        using namespace std::chrono;
+        static const auto start_time = steady_clock::now();
+        const auto elapsed_time = steady_clock::now() - start_time;
+        return duration_cast<milliseconds>(elapsed_time).count();
 //        return std::chrono::system_clock::now().
         //return GetTickCount64();
         //return static_cast<uint64_t>(*(float *) Offsets::oGameTime * 1000);
