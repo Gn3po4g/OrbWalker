4c450e06d16ec7b1591187ceef891f88fbd6d51f
noroby <gnepoag@outlook.com>
2023-07-10 14:49:35 +0800

    fix bug

--- README.md
+++ README.md
@@ -1,17 +1,14 @@
-<h1 style="text-align: center;">
-    Supported Version: 13.13
-</h1>
+<h1 align="center">Supported Version: 13.13</h1>
 
 # Build:
 
-+ Using Visual Studio 2022(for c++20 Module support).
-+ Add Cmake in VS.
-+ Build the project with Release instead of Debug(You need to build several times because the modules depend on each other and all the modules should be compiled).
++ Use Visual Studio 2022.
++ Add CMake and C++ Modules in VS.
++ Change GAME_DIRECTORY in CMakeLists.txt to your game installation directory.
++ Build the project with Release instead of Debug(You need to build several times because the modules depend on each
+  other and all the modules should be compiled).
++ The output file will automatically be generated into Game folder.
 
 # Usage:
 
-**Use in any of the following ways：**
-
-1. Download Hid.dll and put it into your Game folder(e.g. xx\Riot Games\League of Legends\Game).
-2. Build the project and inject the output file(OrbWalker.dll) into League of Legends.exe with an injector.
-3. Build the project and rename the output file to Hid.dll. Then put it into your Game folder.
+Download Hid.dll and put it into your Game folder(e.g. xx\Riot Games\League of Legends\Game).
--- offset.cpp
+++ offset.cpp
@@ -9,26 +9,26 @@ import std.core;
 using namespace std;
 
 struct {
-	uintptr_t *what;
+	uintptr_t &reference;
 	string pattern;
 	uintptr_t addition;
 } sig_to_scan[] = {
-		{&offset.oGameTime,         "F3 0F 5C 35 ? ? ? ? 0F 28 F8",          4}, //ok
-		{&offset.oLocalPlayer,      "48 8B 05 ? ? ? ? 4C 8B D2 4C 8B C1",    3}, //ok
-		{&offset.oViewProjMatrices, "48 8D 0D ? ? ? ? 0F 10 00",             3}, //ok
-		{&offset.oHeroList,         "48 8B 05 ? ? ? ? 4C 8B 78 08",          3}, //ok
-		{&offset.oTurretList,       "48 8B 1D ? ? ? ? 48 8B 5B 28 48 85 DB", 3}, //ok
-		//{ &offsets.oInhibitorList, "A1 ? ? ? ? 53 55 56 8B 70 04 8B 40 08", true },
-		{&offset.oMinionList,       "48 8B 0D ? ? ? ? E8 ? ? ? ? EB 07",     3}, //ok
-		{&offset.oChatClient,       "41 FF D1 48 8B 0D ? ? ? ?",             6},
-		{&offset.oHudInstance,      "48 8B 0D ? ? ? ? 8B 57 10",             3}, //ok
+	{offset.oGameTime,         "F3 0F 5C 35 ? ? ? ? 0F 28 F8",          4}, //ok
+	{offset.oLocalPlayer,      "48 8B 05 ? ? ? ? 4C 8B D2 4C 8B C1",    3}, //ok
+	{offset.oViewProjMatrices, "48 8D 0D ? ? ? ? 0F 10 00",             3}, //ok
+	{offset.oHeroList,         "48 8B 05 ? ? ? ? 4C 8B 78 08",          3}, //ok
+	{offset.oTurretList,       "48 8B 1D ? ? ? ? 48 8B 5B 28 48 85 DB", 3}, //ok
+	//{ &offsets.oInhibitorList, "A1 ? ? ? ? 53 55 56 8B 70 04 8B 40 08", true },
+	{offset.oMinionList,       "48 8B 0D ? ? ? ? E8 ? ? ? ? EB 07",     3}, //ok
+	{offset.oChatClient,       "41 FF D1 48 8B 0D ? ? ? ?",             6},
+	{offset.oHudInstance,      "48 8B 0D ? ? ? ? 8B 57 10",             3}, //ok
 
-		{&offset.oPrintChat,        "E8 ? ? ? ? 4C 8B C3 B2 01",             1},
-		{&offset.oIssueOrder,       "45 33 C0 E8 ? ? ? ? 48 83 C4 48",       4}, //ok
-		{&offset.oAttackDelay,      "E8 ? ? ? ? 33 C0 F3 0F 11 83 ? ? ? ?",  1}, //ok
-		{&offset.oAttackCastDelay,  "89 83 ? ? ? ? E8 ? ? ? ? 48 8B CE",     7}, //ok
-		{&offset.oIsAlive,          "48 8B D8 E8 ? ? ? ? 84 C0 74 35",       4}, //ok
-		{&offset.oBonusRadius,      "E8 ? ? ? ? 0F 28 F8 48 8B D3 48 8B CE", 1} //ok
+	{offset.oPrintChat,        "E8 ? ? ? ? 4C 8B C3 B2 01",             1},
+	{offset.oIssueOrder,       "45 33 C0 E8 ? ? ? ? 48 83 C4 48",       4}, //ok
+	{offset.oAttackDelay,      "E8 ? ? ? ? 33 C0 F3 0F 11 83 ? ? ? ?",  1}, //ok
+	{offset.oAttackCastDelay,  "89 83 ? ? ? ? E8 ? ? ? ? 48 8B CE",     7}, //ok
+	{offset.oIsAlive,          "48 8B D8 E8 ? ? ? ? 84 C0 74 35",       4}, //ok
+	{offset.oBonusRadius,      "E8 ? ? ? ? 0F 28 F8 48 8B D3 48 8B CE", 1} //ok
 };
 
 vector <pair<uint8_t, bool>> convertStringToVector(const string &input) {
@@ -48,21 +48,19 @@ vector <pair<uint8_t, bool>> convertStringToVector(const string &input) {
 
 uintptr_t FindAddress(const string &pattern) {
 	auto moduleAddr = GetModuleHandle(nullptr);
-	MEMORY_BASIC_INFORMATION memInfo;
+	MEMORY_BASIC_INFORMATION mbi;
 	auto *baseAddress = (uint8_t *) moduleAddr;
 	auto patternArr = convertStringToVector(pattern);
-	while (VirtualQuery(baseAddress, &memInfo, sizeof(memInfo))) {
-		if (memInfo.Protect != PAGE_NOACCESS) {
-			auto startAddr = (uint8_t *) memInfo.BaseAddress, endAddr = startAddr + memInfo.RegionSize;
+	while (VirtualQuery(baseAddress, &mbi, sizeof(mbi))) {
+		if (mbi.Protect != PAGE_NOACCESS) {
+			auto startAddr = (uint8_t *) mbi.BaseAddress, endAddr = startAddr + mbi.RegionSize - 1;
 			auto result = search(startAddr, endAddr, patternArr.begin(), patternArr.end(),
 			                     [](uint8_t a, pair<uint8_t, bool> b) {
 				                     return !b.second || a == b.first;
 			                     });
-			if (result != endAddr) {
-				return (uintptr_t) result;
-			}
+			if (result != endAddr) return (uintptr_t) result;
 		}
-		baseAddress = baseAddress + memInfo.RegionSize;
+		baseAddress += mbi.RegionSize;
 	}
 	return 0;
 }
@@ -76,6 +74,6 @@ void InitOffset() {
 			address = FindAddress(pattern);
 		}
 		address += addition;
-		*what = address + 4 + *(int32_t *) address;
+		what = address + 4 + *(int32_t *) address;
 	}
 }
